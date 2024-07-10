6ad12062a7901f6958190e260def8565e7635431
Lautaro Aguilar <97558288+lautaroAguilar@users.noreply.github.com>
2024-07-10 10:05:38 -0300

    create header, add into page and add background image

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,27 @@
-import styles from './page.module.css'
-import Landing from '../containers/landing/Landing'
-import About from "../containers/about/About"
-import Footer from '../containers/footer/Footer'
-import Review from '@/containers/review/Review'
-import Promo from "@/containers/promo/Promo"
+import styles from "./page.module.css";
+import background from "../../public/Fastback.jpg";
+import Landing from "../containers/landing/Landing";
+import About from "../containers/about/About";
+import Footer from "../containers/footer/Footer";
+import Review from "@/containers/review/Review";
+import Promo from "@/containers/promo/Promo";
+import Header from "@/components/Header/Header";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className={styles.main} id='root'>
-      <Landing/>
-      <About/>
-      <Promo/>
-      <Review/>
-      <Footer/>
+    <main className={styles.main} id="root">
+      <Header />
+      <Landing />
+      <About />
+      <Promo />
+      <Review />
+      <Footer />
+      <Image
+        className={styles.background}
+        src={background}
+        alt="background iamge"
+      />
     </main>
-  )
+  );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from "./Header.module.css";
+import fiatGiama from "../../../public/fiatGiama.svg";
+import { fontTitle } from "@/app/font";
+import Link from "next/link";
+import Image from "next/image";
+import Button from "../Button/Button";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container_logo}>
+        <Image
+          src={fiatGiama}
+          alt="logo de Fiat Giama"
+          className={styles.giama}
+        />
+        <div className={styles.divider}></div>
+        <h1 className={`${styles.title} ${fontTitle.className}`}>Leandro Aguilar</h1>
+      </div>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link href={"#"} className={`${styles.nav_options} ${fontTitle.className}`}>Quienes Somos</Link>
+          </li>
+          <li>
+            <Link href={"#"} className={`${styles.nav_options} ${fontTitle.className}`}>Cotizar</Link>
+          </li>
+          <li>
+            <Link href={"#"} className={`${styles.nav_options} ${fontTitle.className}`}>Contactar Asesor</Link>
+          </li>
+          <li>
+            <Link href={"#"} className={`${styles.nav_options} ${fontTitle.className}`}>Catálogo</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.container_cta}>
+        <Button text={"Contactar"} />
+      </div>
+    </header>
+  );
+}
